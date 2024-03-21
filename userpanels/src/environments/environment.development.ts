38ab1552e88060102022656031725a8fb0d8aca5
Inzamul Hoque <inzamrzn918@gmail.com>
2024-03-21 09:58:44 -0700

    fix env

---
+++ userpanels/src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    baseUrl: 'https://winclub247.com/api/v1'
+};
---
+++ userpanels/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://winclub247.com/api/v1'
+
+};

3162f9620ed2edaf50118723fb99293a63d9557d
Shahid <shahidwazidh@gmail.com>
2024-03-21 23:52:27 +0530

    exchange_url link fix

--- userpanels/src/environments/environment.development.ts
+++ userpanels/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
     // baseUrl: 'https://winclub247.com/api/v1'
-    baseUrl: 'http://localhost:8000/api/v1'
+    baseUrl: 'http://localhost:9000/api/v1'
 };

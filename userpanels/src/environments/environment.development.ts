0f596caf034ae70b099ca3aa32c36b779ee7e7e7
Inzamul Hoque <inzamrzn918@gmail.com>
2024-03-21 10:01:11 -0700

    env

--- userpanels/src/environments/environment.development.ts
+++ userpanels/src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    baseUrl: 'https://winclub247.com/api/v1'
+    // baseUrl: 'https://winclub247.com/api/v1'
+    baseUrl: 'http://localhost:8000/api/v1'
 };
